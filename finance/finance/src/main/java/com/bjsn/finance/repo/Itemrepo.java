21c36b5b5c5e76a7727a6f788251907bc25f3b8b
nithyanandham <230167.it@rmkec.ac.in>
2025-08-19 18:46:55 +0530

    Delete finance directory

--- finance/finance/src/main/java/com/bjsn/finance/repo/Itemrepo.java
+++
@@ -1,7 +0,0 @@
-package com.bjsn.finance.repo;
-
-import com.bjsn.finance.Module.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Itemrepo extends JpaRepository<Item,Integer> {
-}
